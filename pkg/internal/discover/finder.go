0d4388007fb4be7fc79c3c92e62fadb6e49c2713
Rafael Roquetto <rafael.roquetto@grafana.com>
2024-10-18 13:55:57 -0600

    WIP

--- pkg/internal/discover/finder.go
+++ pkg/internal/discover/finder.go
@@ -10,6 +10,7 @@ import (
 	"github.com/grafana/beyla/pkg/internal/ebpf"
 	"github.com/grafana/beyla/pkg/internal/ebpf/generictracer"
 	"github.com/grafana/beyla/pkg/internal/ebpf/gotracer"
+	"github.com/grafana/beyla/pkg/internal/ebpf/httptracer"
 	"github.com/grafana/beyla/pkg/internal/ebpf/tctracer"
 	"github.com/grafana/beyla/pkg/internal/imetrics"
 	"github.com/grafana/beyla/pkg/internal/pipe/global"
@@ -99,7 +100,7 @@ func newGoTracersGroup(cfg *beyla.Config, metrics imetrics.Reporter) []ebpf.Trac
 
 func newGenericTracersGroup(cfg *beyla.Config, metrics imetrics.Reporter) []ebpf.Tracer {
 	if cfg.EBPF.UseLinuxTC {
-		return []ebpf.Tracer{generictracer.New(cfg, metrics), tctracer.New(cfg)}
+		return []ebpf.Tracer{generictracer.New(cfg, metrics), tctracer.New(cfg), httptracer.New(cfg)}
 	}
 
 	return []ebpf.Tracer{generictracer.New(cfg, metrics)}
